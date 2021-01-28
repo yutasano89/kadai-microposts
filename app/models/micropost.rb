163d74822a72a2d9be774d4a9188bb73d881d04d
yuta.sano <yutasano89@gmail.com>
2021-01-28 13:03:50 +0000

    likes-rev2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,6 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :fav_users, through: :favorites, source: :user
 end
